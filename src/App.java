b23fe07272465e0078c814ed35b8c066153eafcd
Koya Yamauchi <e245737@ie.u-ryukyu.ac.jp>
2024-12-11 00:39:22 +0900

    report5_step2

--- src/App.java
+++ src/App.java
@@ -1,6 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
         String str = null;
+        
+        try{
         System.out.println(str.length());
+        
+        }catch (NullPointerException e){
+            System.out.println("NullPointerException が発生しました");
+            System.out.println("例外メッセージ："+ e.getMessage());
+        }
     }
 }
